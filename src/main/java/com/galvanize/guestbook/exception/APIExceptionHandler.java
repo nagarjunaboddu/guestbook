ba0826c95e3e7c0bd79c8ab5562d6f67d1cd109f
uthircloudnative <uthircloudnative@gmail.com>
2021-01-23 17:15:14 -0500

    Exception Handling implementation

---
+++ src/main/java/com/galvanize/guestbook/exception/APIExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.galvanize.guestbook.exception;
+
+import com.galvanize.guestbook.model.GuestBoolError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class APIExceptionHandler  {
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<Object> handleAppException(APIException apiException){
+        //APIException newApiException = new APIException(apiException.getErrCode(),apiException.getErrMsg());
+        GuestBoolError guestBoolError = new GuestBoolError(apiException.getErrCode(),apiException.getErrMsg());
+        return new ResponseEntity<>(guestBoolError, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
